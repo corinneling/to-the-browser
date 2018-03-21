78bdd4b409a99d2480715a7d61dc8225b2e196db
corinneling <corinne@heysparkbox.com>
2018-03-21 15:10:03 -0400

    add describte to test file

--- test/triangle-test.js
+++ test/triangle-test.js
@@ -1,9 +1,18 @@
 const expect = require("chai").expect;
-const sinon = require('sinon');
-var EventEmitter = require('events').EventEmitter;
-const app = require('../app/script.js')
+const sinon = require("sinon");
 
 document = {
-  querySelectorAll: function(){ return null; },
-  getElementById: function(){ return null; },
+  querySelectorAll: function() {
+    return null;
+  },
+  getElementById: function() {
+    return null;
+  }
 };
+
+describe("Triangle", function() {
+  it("will see if test runs", function() {
+    const app = require("../app/script.js");
+    assert(true);
+  });
+});
