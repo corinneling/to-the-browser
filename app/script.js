749ffb5e591788aa4937be95c89c5d6a506bb6d2
corinneling <corinne@heysparkbox.com>
2018-03-21 15:03:57 -0400

    add factory functions
    still trying to figure out testig for this

--- app/script.js
+++ app/script.js
@@ -7,52 +7,56 @@ let triangle = {
   answer: document.getElementById("triangleType")
 };
 
-const startListeners = _ => {
-  triangle.btn.addEventListener("click", showAnswer);
-  getRanges();
+const CreateRangesFunctionality = () => {
+  const getRanges = () => {
+    triangle.inputs.forEach(i => {
+      i.addEventListener("input", showRangeValues);
+    });
+  };
+
+  const showRangeValues = () => {
+    for (var i = 0; i < triangle.inputs.length; i++) {
+      triangle.outputs[i].value = triangle.inputs[i].value;
+    }
+  };
+  return { getRanges };
 };
 
-const getRanges = _ => {
-  triangle.inputs.forEach(i => {
-    i.addEventListener("input", showRangeValues);
-  });
+const CreateTriangle = () => {
+  const getInputValues = () =>
+    Array.from(triangle.inputs).map(inputs => inputs.value);
+
+  const classifyTriangle = () => {
+    let [side1, side2, side3] = getInputValues();
+
+    let type = null;
+    if (side1 == side2 && side2 == side3) {
+      type = "equilateral";
+    } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+      type = "isosceles";
+    } else {
+      type = "scalene";
+    }
+    return type;
+  };
+
+  const addTypeToString = () => {
+    let triangle_type = classifyTriangle();
+    triangle.answer.innerHTML =
+      "You created " + a(`${triangle_type}`) + " triangle";
+  };
+
+  const showAnswer = () => {
+    triangle.btn.addEventListener("click", addTypeToString);
+  };
+
+  return { showAnswer };
 };
 
-const showRangeValues = _ => {
-  for (var i = 0; i < triangle.inputs.length; i++) {
-    triangle.outputs[i].value = triangle.inputs[i].value;
-  }
-};
+const visible_ranges = CreateRangesFunctionality();
+const which_triangle = CreateTriangle();
 
-const getInputValues = _ =>
-  Array.from(triangle.inputs).map(inputs => inputs.value);
-
-const classifyTriangle = _ => {
-  let [side1, side2, side3] = getInputValues();
-
-  let type = null;
-  if (side1 == side2 && side2 == side3) {
-    type = "equilateral";
-  } else if (side1 == side2 || side2 == side3 || side1 == side3) {
-    type = "isosceles";
-  } else {
-    type = "scalene";
-  }
-  return type;
-};
-
-const showAnswer = _ => {
-  let triangle_type = classifyTriangle();
-  triangle.answer.innerHTML =
-    "You created " + a(`${triangle_type}`) + " triangle";
-};
-
-startListeners();
-
-module.exports = classifyTriangle();
-
-// questions:
-/*
-  1. how does bundle.js work? do you have to run npx webpack everytime you make an update
-  2. whats the difference bw dependencies and devDependencies
-*/
+(function startListeners() {
+  which_triangle.showAnswer();
+  visible_ranges.getRanges();
+})();
--- test/triangle-test.js
+++ test/triangle-test.js
@@ -1,9 +1,9 @@
-const expect = require('chai').expect;
-const tri = require('../app/script.js');
-const values = [1, 2, 3];
+const expect = require("chai").expect;
+const sinon = require('sinon');
+var EventEmitter = require('events').EventEmitter;
+const app = require('../app/script.js')
 
-describe('Triangle', function(){
-  it('returns equilateral for 3 equal sides', function(){
-    expect(tri()).to.equal('equilateral');
-  })
-})
+document = {
+  querySelectorAll: function(){ return null; },
+  getElementById: function(){ return null; },
+};
