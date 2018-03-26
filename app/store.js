21dbeffcf1818fe9a96ce61f0784a086b9807ffc
corinneling <corinne@heysparkbox.com>
2018-03-26 10:24:51 -0400

    remove unnecessary item

--- app/store.js
+++ app/store.js
@@ -1,6 +1,5 @@
 export const form = {
   inputs: document.querySelectorAll("input[type=range]"),
   outputs: document.querySelectorAll("output"),
-  btn: document.getElementById("submitBtn"),
   answer: document.getElementById("triangleType")
 };
