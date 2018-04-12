cde17adcbfd88289cb2003e881e031164743c6a5
corinneling <corinne@heysparkbox.com>
2018-04-12 09:52:50 -0400

    move order functions
    to another files

--- hot-dog-stand/app/script.js
+++ hot-dog-stand/app/script.js
@@ -6,7 +6,7 @@ const adminForm = require('./admin/create_form.js'),
   bunInventory = require('./admin/render_buns.js'),
   optionsByArea = require('./order/render_ingredients.js'),
   formSumbit = require('./order/form_submit.js');
-
+  
 (function startApp() {
   // create admin form
   adminForm();
